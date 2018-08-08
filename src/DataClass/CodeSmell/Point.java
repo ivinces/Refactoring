8b7bcfbc9011ce659428d03759b944e5a683037d
ivinces <ivinces@espol.edu.ec>
2018-08-08 15:14:55 -0500

    Data Class

---
+++ src/DataClass/CodeSmell/Point.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataClass.CodeSmell;
+
+public class Point {
+    int x;
+    int y;
+    
+    Point(){
+        this.x=0;
+        this.y=0;
+    }
+}
