759eb6d13e44a112dfed25e9074539c67f4cc8d0
ivinces <ivinces@espol.edu.ec>
2018-08-08 21:31:10 -0500

    Inappropiate Intimacy

---
+++ src/InappropriateIntimacy/CodeSmell/Nombre.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InappropriateIntimacy.CodeSmell;
+
+/**
+ *
+ * @author http://siloracle.blogspot.com/2014/11/code-smells-inappropriate-intimacy-with.html
+ */
+public class Nombre {
+    public String name;
+}
---
+++ src/InappropriateIntimacy/Refactoring/Nombre.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InappropriateIntimacy.Refactoring;
+
+/**
+ *
+ * @author http://siloracle.blogspot.com/2014/11/code-smells-inappropriate-intimacy-with.html
+ */
+public class Nombre {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
